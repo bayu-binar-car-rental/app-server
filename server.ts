8cd3fc42d64229eb9672bd683f933ab53c318b5c
TamaWira <asusrog20012001@gmail.com>
2024-02-22 23:22:23 +0800

    feat: initialize routes for CRUD REST API

--- server.ts
+++ server.ts
@@ -1,14 +1,42 @@
 import express, { Express, Request, Response } from "express";
-import path = require("path");
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const app: Express = express();
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Typescript server is running...");
+app.use(express.urlencoded());
+
+// List
+app.get("/", (_, res: Response) => {
+  res.send("GET: List");
+});
+
+// Find
+app.get("/:id", (req: Request, res: Response) => {
+  const id = +req.params.id;
+  res.send(`GET: Find ID ${id}`);
+});
+
+// Create
+app.post("/", (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+  res.send(`POST: Create => ${JSON.stringify(body)}`);
+});
+
+// Update
+app.patch("/:id", (req: Request, res: Response) => {
+  const id = +req.params.id;
+  res.send(`PATCH: Update ID ${id}`);
+});
+
+// Delete
+app.delete("/:id", (req: Request, res: Response) => {
+  const id = +req.params.id;
+  res.send(`DELETE: Delete ID ${id}`);
 });
 
 app.listen(PORT, () =>
